b00e5ab6955f7a3bab80a152de7471742e5af1d1
shreyes-pd1 <mca.2204@unigoa.ac.in>
2022-12-22 01:01:46 +0530

    initialization of board

--- snake_and_ladder_2/src/board/Board.java
+++ snake_and_ladder_2/src/board/Board.java
@@ -1,20 +1,50 @@
 package board;
 
+import java.util.HashMap;
 import java.util.List;
 
 import dataLoadUtility.DataLoadUtility;
+import entities.BoardEntity;
+import entities.Ladder;
+import entities.Snake;
 
 public class Board {
-	List<String> wordList;
-	public Board() {
-		this.wordList = DataLoadUtility.dataload("./words");
-	}
+
+	HashMap<Integer, BoardEntity> cells;
+	Integer cellCount;
 	
+	public Board(int dimention) {
+		this.cellCount = dimention*dimention;
+		cells = new HashMap<Integer, BoardEntity>();
+		for(int i=0; i<dimention; i++) {
+			Integer min = 2;
+			Integer max = cellCount - 1;
+			Integer start = (int) Math.floor(Math.random()*(max-min+1)+min);
+			max = start - 1;
+			Integer end = (int) Math.floor(Math.random()*(max-min+1)+min);
+			if(!hasBoardEntity(start)) {
+				setEntity(start, new Snake());
+			}
+			max = cellCount - 1;
+			end = (int) Math.floor(Math.random()*(max-min+1)+min);
+			max = end - 1;
+			start = (int) Math.floor(Math.random()*(max-min+1)+min);
+			if(!hasBoardEntity(start)) {
+				setEntity(start, new Ladder());
+			}
+			
+		}
+	}
     
 	public void print() {
-		for (String word:wordList) {
-    		System.out.println(word);
-    	}
-		 
+	
+	}
+
+	private void setEntity(int index, BoardEntity e) {
+		cells.put(index, e);
+	}
+
+	public boolean hasBoardEntity(int cellIndex) {
+		return cells.containsKey(cellIndex);
 	}
 }
