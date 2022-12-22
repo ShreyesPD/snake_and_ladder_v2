9d809be22cfc9430eb6ac400184123435224e6d5
2207-joseito <115062378+2207-joseito@users.noreply.github.com>
2022-12-22 08:35:27 +0000

    created makemove method for game

--- snake_and_ladder_2/src/game/Game.java
+++ snake_and_ladder_2/src/game/Game.java
@@ -4,27 +4,56 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 import board.Board;
+import entities.BoardEntity;
 import entities.Player;
 import word_find.WordFind;
 
-
-public class Game{
-    Board board;
+public class Game {
+	Board board;
 	Queue<Player> players;
 	Queue<Player> winners;
 	int size = 100;
 	WordFind wordFinder;
-	
+
 	public Game() {
 		this.wordFinder = new WordFind();
 
 		board = new Board(10);
 		players = new LinkedList<Player>();
 		winners = new LinkedList<Player>();
-		
+
 	}
 
-    public Queue<Player> getPlayers() {
+	private void makeMove(Player player, int move) {
+		int currPosition = player.getPosition();
+
+		if (move == 0) {
+			System.out.println("Word Not Found!");
+			return;
+		}
+
+		System.out.println("You got: " + move);
+		int finalPos = currPosition + move;
+		if (finalPos <= size) {
+			if (board.hasBoardEntity(finalPos)) {
+				BoardEntity boardEntity = board.getEntity(finalPos);
+
+				if (boardEntity.getStart() == boardEntity.getEnd()) {
+					boardEntity.setEnd(boardEntity.getStart() + (move * 2));
+				}
+				System.out.println(boardEntity.getEncounterMessage());
+				finalPos = boardEntity.getEnd();
+			}
+			System.out.println("Taking you to " + finalPos);
+		} else {
+			finalPos = this.size;
+		}
+
+		player.setPosition(finalPos);
+
+	}
+
+	public Queue<Player> getPlayers() {
 		return players;
 	}
 
