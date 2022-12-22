70921ccd87fe5a61653c43a46d1e2241bf0826ad
laxita-n <117807087+laxita-n@users.noreply.github.com>
2022-12-22 06:04:56 +0000

    added player class

---
+++ snake_and_ladder_2/src/entities/Player.java
@@ -0,0 +1,45 @@
+
+import entities.BoardEntity;
+
+public class Player 
+{
+    Integer position;
+	String userName;
+	
+	
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public Player(String userName) {
+		
+		position = 0;
+		this.userName = userName; 
+	}
+
+	public int getPosition() {
+		return position;
+	}
+
+	public void setPosition(int position) {
+		this.position = position;
+	}
+
+	@Override
+	public String getEncounterMessage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getString() {
+		// TODO Auto-generated method stub
+		return userName;
+	}
+
+
+}
