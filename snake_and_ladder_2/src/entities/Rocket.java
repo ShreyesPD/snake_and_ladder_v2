ed3e339f730d76d7afc277153ab19a3fc0d43d0a
2207-joseito <115062378+2207-joseito@users.noreply.github.com>
2022-12-22 07:24:08 +0000

    created rocket entity

---
+++ snake_and_ladder_2/src/entities/Rocket.java
@@ -0,0 +1,17 @@
+package entities;
+
+public class Rocket extends BoardEntity {
+    public Rocket(int position) {
+        super(position, position);
+    }
+
+    @Override
+    public String getEncounterMessage() {
+        return "Yeyyy!! You got a rocket :) ";
+    }
+
+    @Override
+    public String getString() {
+        return "🚀 ";
+    }
+}
