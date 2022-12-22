78be46f861563b356955ccc10f2493ec812af1c8
2201-safwan <117520007+safwan-shaikh@users.noreply.github.com>
2022-12-22 05:31:43 +0000

    Added class Hint

---
+++ snake_and_ladder_2/src/entities/Hint.java
@@ -0,0 +1,21 @@
+package entities;
+
+public class Hint extends BoardEntity{
+
+    public Hint(int start, int end) {
+        super(start, end);
+    }
+
+    @Override
+    public String getEncounterMessage() {
+       return "You got a hint";
+        
+    }
+
+    @Override
+    public String getString() {
+        
+        return "H";
+    }
+    
+}
